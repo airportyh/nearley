542f640b450bae7fa86067d8483762b166499fb1
Toby Ho <airportyh@gmail.com>
2019-07-18 15:25:59 -0400

    WIP

--- bin/error-report-experiment.js
+++ bin/error-report-experiment.js
@@ -1,15 +1,31 @@
 const nearley = require("../lib/nearley");
-const grammar = require("../examples/js/csscolor");
 
-main();
 
-function main() {
+json();
+
+function cssColor() {
+    const grammar = require("../examples/js/csscolor");
     const parser = new nearley.Parser(
         nearley.Grammar.fromCompiled(grammar),
         { keepHistory: false }
     );
     try {
         parser.feed("b4d4ss");
+        console.log("results", parser.results);
+    } catch (e) {
+        console.log(e.message);
+    }
+}
+
+function json() {
+    const grammar = require("../examples/js/json.js");
+    const parser = new nearley.Parser(
+        nearley.Grammar.fromCompiled(grammar),
+        { keepHistory: false }
+    );
+    try {
+        const code = `[abc]`;
+        parser.feed(code);
         console.log("results", parser.results);
     } catch (e) {
         console.log(e.message);
